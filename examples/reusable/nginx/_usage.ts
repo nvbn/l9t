bc0e8ea444f6b1e6c5bb9f475843a7940b093b93
Vladimir Iakovlev <nvbn.rm@gmail.com>
2020-06-15 22:40:07 +0200

    Use github import in nginx example usage

--- examples/reusable/nginx/_usage.ts
+++ examples/reusable/nginx/_usage.ts
@@ -1,5 +1,5 @@
 import l9t from "https://raw.githubusercontent.com/nvbn/l9t/master/l9t.ts";
-import nginx from "./index.ts";
+import nginx from "https://raw.githubusercontent.com/nvbn/l9t/master/examples/reusable/nginx/index.ts";
 
 l9t([...nginx({
   name: "example-nginx-usage",
